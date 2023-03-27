029af616b2046aeab67495d485c7652a63707876
taconeko <shahlalalalala@gmail.com>
2023-03-27 17:10:00 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,75 @@
+# Frontend Mentor - Interactive rating component solution
+
+This is a solution to the [Interactive rating component challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/interactive-rating-component-koxpeBUmI). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+## Overview
+
+I tried to focus too much on making the site accessible. I don't know if I did well. I could definitely have achieved this in a much easier way and way quicker, if I didn't care. Unfortunately, I do.
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Select and submit a number rating
+- See the "Thank you" card state after submitting a rating
+
+### Screenshot
+
+![](./images/screenshot1.png)
+![](./images/screenshot2.png)
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+I spent way too much time on this. Almost a whole day. I ADHD hyperfixated on it and forgot to eat or sleep, and it's still not perfect. But I guess it's something a lot of people won't notice...
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Mobile-first workflow
+- Sass
+- Javascript
+
+### What I learned
+
+I learnt that I am very very tired and sleep deprived and I am going to bed.
+
+I learnt a lot about tabindex, though. That gave me hell when trying to make this accessible. It's still not perfect... the hover states don't carry over very well from mouse to keyboard navigation. There's only so much I can do though without writing insane amounts of code.
+
+### Continued development
+
+I want to learn how to refactor my code properly. I don't know how to do it well without breaking it. I had to ask ChatGPT for advice. You know I'm desperate when I go to an AI that might end my career before it begins, for help.
+
+### Useful resources
+
+- ChatGPT. I do not like you much, but you helped. This time.
+
+## Author
+
+- Frontend Mentor - [@taco-neko](https://www.frontendmentor.io/profile/taco-neko)
+
+## Acknowledgments
+
+- Dirtblort for telling me cool knife facts
---
+++ script.js
@@ -0,0 +1,126 @@
+//Handle tabIndex and blur
+
+const ratingBtns = document.querySelectorAll(".rating__button button");
+const firstBtn = ratingBtns[ratingBtns.length - 1]
+const firstListItem = firstBtn.parentElement
+const lastBtn = ratingBtns[0]
+const lastListItem = lastBtn.parentElement
+const submitBtn = document.querySelector(".submit-button")
+
+ratingBtns.forEach((btn) => btn.tabIndex = -1)
+firstBtn.tabIndex = 0
+
+function focusNextBtn(e) {
+  const listItem = e.target.parentElement
+
+  if (e.key === "Tab" && !e.shiftKey) {
+    e.preventDefault()
+
+    const nextListItem = listItem.previousElementSibling || submitBtn 
+    const nextBtn = nextListItem.querySelector("button") || submitBtn
+
+    nextBtn.tabIndex = 0
+    nextBtn.focus()
+
+    ratingBtns.forEach((btn) => {
+      if (btn !== nextBtn) btn.tabIndex = -1
+    })
+  }
+  else if (e.key === "Tab" && e.shiftKey) {
+    e.preventDefault()
+
+    const prevListItem = listItem.nextElementSibling || lastListItem
+    const prevBtn = prevListItem.querySelector("button")
+
+    prevBtn.tabIndex = 0
+    prevBtn.focus()
+
+    ratingBtns.forEach((btn) => {
+      if (btn !== prevBtn) btn.tabIndex = -1
+    })
+  }
+}
+
+ratingBtns.forEach((btn) => btn.addEventListener("keydown", focusNextBtn))
+
+function resetTabIndexes() {
+  ratingBtns.forEach((btn) => {
+    btn.tabIndex = -1
+    firstBtn.tabIndex = 0
+  })
+}
+
+ratingBtns.forEach((btn) => btn.addEventListener("blur", resetTabIndexes))
+
+submitBtn.addEventListener("keydown", (e) => {
+  if (e.key === "Tab" && e.shiftKey) {
+    e.preventDefault()
+    lastBtn.tabIndex = 0
+    lastBtn.focus()
+  }
+})
+
+submitBtn.addEventListener("blur", resetTabIndexes)
+
+// Styling the rating buttons on click and hover
+
+function generateThanksHTML(rating) {
+  return `
+  <div class="container thanks-state">
+    <img class="thanks-image" src="/images/illustration-thank-you.svg">
+    <p class="selected">You selected ${rating} out of 5</p>
+    <h1>Thank you!</h1>
+    <p>We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!</p>
+  </div>`
+}
+
+let thanksHTML = ""
+
+let ratingChosen = false
+
+ratingBtns.forEach((btn, index1) => {
+  //click
+  btn.addEventListener("click", () => {
+    ratingBtns.forEach((btn, index2) => {
+      index1 <= index2 ? btn.classList.add("active") : btn.classList.remove("active")
+      thanksHTML = generateThanksHTML(ratingBtns[index1].textContent)
+      btn.blur()
+      ratingChosen = true
+    })
+    
+  })
+  //hover
+  btn.addEventListener("mouseover", () => {
+    ratingBtns.forEach((btn, index2) => {
+      index1 <= index2 ? btn.classList.add("hovered") : btn.classList.remove("hovered")
+      btn.blur()
+    })
+  })
+  //mouse out
+  btn.addEventListener("mouseout", () => {
+    ratingBtns.forEach((btn, index2) => {
+      if (index1 <= index2) {
+        btn.classList.remove("hovered")
+      }
+      btn.blur()
+    })
+  })
+})
+
+function logcurrent() {
+  console.log(currentRating)
+}
+
+// Switching state on submit
+
+const main = document.querySelector("main")
+
+submitBtn.addEventListener("click", () => {
+  if (ratingChosen) {
+    main.innerHTML = thanksHTML
+  }  
+})
+
+
+
+
